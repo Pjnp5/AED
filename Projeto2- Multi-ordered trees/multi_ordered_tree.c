e1f6e2f93ebb05d4d3381de93ad18cd388e5520d
Raquel Paradinha <raquelparadinha@gmail.com>
2022-01-20 11:02:27 +0000

    Quase completo

--- Projeto2- Multi-ordered trees/multi_ordered_tree.c
+++ Projeto2- Multi-ordered trees/multi_ordered_tree.c
@@ -18,6 +18,8 @@
 // so, when inserting a new node we need to do it three times (one for each index), so we will end upo with 3 three roots
 //
 
+int ctr = 0;
+
 typedef struct tree_node_s
 {
   char name[ MAX_NAME_SIZE + 1];                         // index 0 data item
@@ -55,53 +57,23 @@ int compare_tree_nodes(tree_node_t *node1,tree_node_t *node2,int main_idx)
 // tree insertion routine (place your code here)
 //
 
-// tree_node_t* new_tree_node(tree_node_t *person){
-//     tree_node_t* newNode = (tree_node_t*)malloc(sizeof(tree_node_t));
-// 
-//     strcpy(newNode->name, person->name);
-//     strcpy(newNode->zip_code, person->zip_code);
-//     strcpy(newNode->telephone_number, person->telephone_number);
-//     *newNode->left = *newNode->right = NULL;
-// 
-//     return newNode;
-// }
-
+// FAVOR NAO MEXER
 void tree_insert(tree_node_t **link, tree_node_t *person , int main_index){
-  if (main_index == 0){
-    // printf("%s\n",person->name);
-    if(*link == NULL){ 
+  // if (main_index == 0){
+  //   printf("%s\n",person->name);
+  // } else if (main_index == 1){
+  //   printf("%s\n",person->zip_code);
+  // } else {
+  //   printf("%s\n",person->telephone_number);
+  // }
+  if(*link == NULL){ 
       (*link) = person;
-    }
-    else if(compare_tree_nodes(*link, person, main_index) > 0){
-        tree_insert(&((*link)->left[main_index]), person, main_index);
-      }
-    else{
-      tree_insert(&((*link)->right[main_index]), person, main_index);
-    }
   }
-  else if (main_index == 1){
-    // printf("%s\n",person->zip_code);
-    if(*link == NULL){ 
-      (*link) = person;
+  else if(compare_tree_nodes(*link, person, main_index) > 0){  
+      tree_insert(&((*link)->left[main_index]), person, main_index); 
     }
-    else if(compare_tree_nodes(*link, person, main_index) > 0){
-        tree_insert(&((*link)->left[main_index]), person, main_index);
-      }
-    else{
-      tree_insert(&((*link)->right[main_index]), person, main_index);
-    }
-  }
-  else {
-    // printf("%s\n",person->telephone_number);
-    if(*link == NULL){ 
-      (*link) = person;
-    }
-    else if(compare_tree_nodes(*link, person, main_index) > 0){
-        tree_insert(&((*link)->left[main_index]), person, main_index);
-      }
-    else{
-      tree_insert(&((*link)->right[main_index]), person, main_index);
-    };
+  else{
+    tree_insert(&((*link)->right[main_index]), person, main_index);
   }
 }
 
@@ -110,29 +82,51 @@ void tree_insert(tree_node_t **link, tree_node_t *person , int main_index){
 // tree search routine (place your code here)
 //
 
-tree_node_t *find( ... ){
+tree_node_t *find(tree_node_t *link, tree_node_t *person, int main_index){
   
+  if(link == NULL || compare_tree_nodes(link, person, main_index) == 0) {
+    return link;
+  }
+  else if(compare_tree_nodes(link, person, main_index) > 0){
+    return find(link->left[main_index],person, main_index);
+  }
+  else {
+    return find(link->right[main_index],person, main_index);
+  }
 }
-
 
 //
 // tree depdth
 //
 
-///int tree_depth( ... )
-///{
-///}
+int tree_depth(tree_node_t *link, int main_index) {
+  if (link == NULL){
+    return 0;
+  }
+  int ld = tree_depth(link->left[main_index], main_index);
+  int rd = tree_depth(link->right[main_index], main_index);
+  if (ld > rd) {return ld + 1;}
+  else {return rd + 1;}
+}
 
 
 //
 // list, i,e, traverse the tree (place your code here)
-
 //
 
-///int list( ... )
-///{
-///}
+int list(tree_node_t *link, int main_index){
+  if(link != NULL){
+    list(link->left[main_index], main_index);
+    ctr++;
+    printf("Person #%d\n",ctr);
+    printf(" name --------------- %s\n",link->name);
+    printf(" zip code ----------- %s\n",link->zip_code);
+    printf(" telephone number --- %s\n",link->telephone_number);
 
+    list(link->right[main_index], main_index);
+  }
+  return EXIT_SUCCESS;
+}
 
 //
 // main program
@@ -141,8 +135,7 @@ tree_node_t *find( ... ){
 int main(int argc,char **argv){
   double dt;
   // process the command line arguments
-  if(argc < 3)
-  {
+  if(argc < 3){
     fprintf(stderr,"Usage: %s student_number number_of_persons [options ...]\n",argv[0]);
     fprintf(stderr,"Recognized options:\n");
     fprintf(stderr,"  -list[N]              # list the tree contents, sorted by key index N (the default is index 0)\n");
@@ -150,27 +143,23 @@ int main(int argc,char **argv){
     return 1;
   }
   int student_number = atoi(argv[1]);
-  if(student_number < 1 || student_number >= 1000000)
-  {
+  if(student_number < 1 || student_number >= 1000000){
     fprintf(stderr,"Bad student number (%d) --- must be an integer belonging to [1,1000000{\n",student_number);
     return 1;
   }
   int n_persons = atoi(argv[2]);
-  if(n_persons < 3 || n_persons > 10000000)
-  {
+  if(n_persons < 3 || n_persons > 10000000){
     fprintf(stderr,"Bad number of persons (%d) --- must be an integer belonging to [3,10000000]\n",n_persons);
     return 1;
   }
   // generate all data
   tree_node_t *persons = (tree_node_t *)calloc((size_t)n_persons,sizeof(tree_node_t)); // arvore para por as pessoas
-  if(persons == NULL)
-  {
+  if(persons == NULL){
     fprintf(stderr,"Output memory!\n");
     return 1;
   }
   aed_srandom(student_number);
-  for(int i = 0;i < n_persons;i++)
-  {
+  for(int i = 0;i < n_persons;i++){
     random_name(&(persons[i].name[0]));
     random_zip_code(&(persons[i].zip_code[0]));
     random_telephone_number(&(persons[i].telephone_number[0]));
@@ -192,44 +181,40 @@ int main(int argc,char **argv){
   printf("Tree creation time (%d persons): %.3es\n",n_persons,dt);
 
   // search the tree
-  ///for(int main_index = 0;main_index < 3;main_index++)
-  ///{
-  ///  dt = cpu_time();
-  ///  for(int i = 0;i < n_persons;i++)
-  ///  {
-  ///    tree_node_t n = persons[i]; // make a copy of the node data
-  ///    if(find( ... ) != &(persons[i])) // place your code here to find a given person, searching for it using the tree with number main_index
-  ///    {
-  ///      fprintf(stderr,"person %d not found using index %d\n",i,main_index);
-  ///      return 1;
-  ///    }
-  ///  }
-  ///  dt = cpu_time() - dt;
-  ///  printf("Tree search time (%d persons, index %d): %.3es\n",n_persons,main_index,dt);
-  ///}
+  for(int main_index = 0;main_index < 3;main_index++){
+    dt = cpu_time();
+    for(int i = 0;i < n_persons;i++){
+      tree_node_t n = persons[i]; // make a copy of the node data
+      if(find(roots[main_index],&(n), main_index) != &(persons[i])) // place your code here to find a given person, searching for it using the tree with number main_index
+      {
+        fprintf(stderr,"person %d not found using index %d\n",i,main_index);
+        return 1;
+      }
+    }
+    dt = cpu_time() - dt;
+    printf("Tree search time (%d persons, index %d): %.3es\n",n_persons,main_index,dt);
+  }
   // compute the largest tree depdth
-  ///for(int main_index = 0;main_index < 3;main_index++)
-  ///{
-  ///  dt = cpu_time();
-  ///  int depth = tree_depth( ... ); // place your code here to compute the depth of the tree with number main_index
-  ///  dt = cpu_time() - dt;
-  ///  printf("Tree depth for index %d: %d (done in %.3es)\n",main_index,depth,dt);
-  ///}
+  for(int main_index = 0;main_index < 3;main_index++){
+    dt = cpu_time();
+    int depth = tree_depth(roots[main_index], main_index); // place your code here to compute the depth of the tree with number main_index
+    dt = cpu_time() - dt;
+    printf("Tree depth for index %d: %d (done in %.3es)\n",main_index,depth,dt);
+  }
   // process the command line optional arguments
-  ///for(int i = 3;i < argc;i++)
-  ///{
-  ///  if(strncmp(argv[i],"-list",5) == 0)
-  ///  { // list all (optional)
-  ///    int main_index = atoi(&(argv[i][5]));
-  ///    if(main_index < 0)
-  ///      main_index = 0;
-  ///    if(main_index > 2)
-  ///      main_index = 2;
-  ///    printf("List of persons:\n");
-  ///    (void)list( ... ); // place your code here to traverse, in order, the tree with number main_index
-  ///  }
+  for(int i = 3;i < argc;i++){
+    if(strncmp(argv[i],"-list",5) == 0){ // list all (optional)
+      int main_index = atoi(&(argv[i][5]));
+      if(main_index < 0)
+        main_index = 0; //Não faz sentido
+      if(main_index > 2)
+        main_index = 2; //Enfim
+      printf("List of persons:\n");
+      (void)list(roots[main_index], main_index); // place your code here to traverse, in order, the tree with number main_index
+    }
     // place your own options here
-  ///}
+    // I wanna commit unlive.
+  }
   // clean up --- don't forget to test your program with valgrind, we don't want any memory leaks
   
   free(persons);
